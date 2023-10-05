9c9bcd1709d78c4825abab4a0e5c579fa8818bf6
paudpez14 <paudpez@hotmail.com>
2023-10-05 07:15:44 -0500

    Changes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,13 +4,6 @@ import { getUsers,insertUser, loginUser  } from "../controller/users.controller.
 
 const router = Router();
 router.use(express.json());
-// Agregar el prefijo "/api" a todas las rutas
-router.use('/api', (req, res, next) => {
-  // Middleware para agregar /api como prefijo a todas las rutas
-  req.url = `/api${req.url}`;
-  next();
-});
-
 // Rutas
 router.get('/users', getUsers);
 router.post('/users/register', insertUser); // Agrega la ruta POST para insertUser
